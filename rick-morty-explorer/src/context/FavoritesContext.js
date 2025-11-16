0cbc33d68fa2163d9d25857e32ad060198882714
rominasula <romina_sula@hotmail.com>
2025-11-15 19:45:29 -0500

    FavoriteContext

--- rick-morty-explorer/src/context/FavoritesContext.js
+++ rick-morty-explorer/src/context/FavoritesContext.js
@@ -5,5 +5,17 @@ export const FavoritesContext = createContext();
 export const FavoritesProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
 
+  const addFavorite = (character) => {
+    setFavorites((prev) => [...prev, character]);
+  };
 
+  const removeFavorite = (id) => {
+    setFavorites((prev) => prev.filter((char) => char.id !== id));
+  };
+
+  return (
+    <FavoritesContext.Provider value={{ favorites, addFavorite, removeFavorite }}>
+      {children}
+    </FavoritesContext.Provider>
+  );
 };
