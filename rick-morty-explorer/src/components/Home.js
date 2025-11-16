ef64fba28a098e51f773d8a0d42297e48fd32020
rominasula <romina_sula@hotmail.com>
2025-11-15 23:37:34 -0500

    fixing home to show the characters

--- rick-morty-explorer/src/components/Home.js
+++ rick-morty-explorer/src/components/Home.js
@@ -1,13 +1,23 @@
 // Home.js
 import React from "react";
+import CharacterList from "./CharacterList";
 import "./Home.css";
 
-export default function Home() {
+export default function Home({ characters }) {
   return (
     <div className="home-page">
       <h1>Welcome to Rick & Morty Explorer!</h1>
-      <p>Click on Characters in the sidebar to browse and add your favorites.</p>
+      <p>Search above or click Characters in the sidebar.</p>
+
+      {/* Show characters ONLY if user typed something */}
+      {characters.length > 0 && (
+        <div className="search-results">
+          <h2>Search Results:</h2>
+          <CharacterList characters={characters} />
+        </div>
+      )}
     </div>
   );
 }
+
 
